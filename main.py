f75803b50fcf2acbbff9c2e74fc7250b1badf144
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2024-03-09 20:07:28 +0700

    fix: code & format response

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import uuid
 app = Flask(__name__)
 
@@ -36,7 +36,7 @@ def update_todo_tags(todos, title, tags):
 # Dapatkan semua to do
 @app.route('/todos', methods=['GET'])
 def GetAllTodos():
-    return todos
+    return jsonify(todos), 200
 
 # Post to do
 @app.route('/todo', methods=['POST'])
@@ -45,32 +45,36 @@ def GetPostTODO():
     data['status'] = data['status'].lower()
 
     if 'title' not in data or 'status' not in data or 'subject' not in data or not data['title'] or not data['status'] or not data['subject']:
-        return {'error': 'title, status, and subject fields are required and must not be empty'}, 400
+        return jsonify({'error': 'title, status, and subject fields are required and must not be empty'}), 400
 
     if data['status'] != 'on progress':
-        return {'error': 'status must be "on progress"'}, 400
+        return jsonify({'error': 'status must be "on progress"'}), 400
 
     if 'tags' not in data:
         data['tags'] = []
 
     data['uid'] = str(uuid.uuid4())
     todos.append(data)
-    return todos, 201
+    return jsonify(todos), 201
 
 # Delete, Centang, dan Dapatkan to do berdasarkan title
 @app.route('/todo/<string:title>', methods=['DELETE', 'GET', 'PUT'])
 def DeletePutTODO(title):
     if request.method == 'GET':
         result = next(filter(lambda x: x.get('title') == title, todos), None)
-        return result if result else [], 404
+        if result:
+            return jsonify(result), 200
+        return jsonify([]), 404
     
     if request.method == 'DELETE':
         if delete_todo(todos, title):
-            return todos
-        return todos, 404
+            return jsonify(todos), 200
+        return jsonify(todos), 404
     
     updated_todo = update_todo_status(todos, title)
-    return updated_todo if updated_todo else updated_todo, 404
+    if updated_todo:
+        return jsonify(updated_todo), 200
+    return jsonify(updated_todo), 404
 
 def is_array_of_strings(arr):
     return isinstance(arr, (list, tuple)) and all(isinstance(item, str) for item in arr)
@@ -78,10 +82,12 @@ def is_array_of_strings(arr):
 def AddTags(title):
     data = request.json
     if not is_array_of_strings(data):
-        return {'error': 'please provide an array of tags'}, 400
+        return jsonify({'error': 'please provide an array of tags'}), 400
 
     res = update_todo_tags(todos, title, data)
-    return res if res else res, 404
+    if res:
+        return jsonify(res), 200
+    return jsonify(res), 404
 
 # Dapatkan semua to do berdasarkan filter tertentu
 @app.route('/todo/filter/<string:type>', methods=['GET'])
@@ -89,16 +95,20 @@ def FilterTodoStatus(type):
     query = request.args.get('q')
 
     if not (type != 'subject' or type != 'status' or type != 'tags'):
-        return {'error': 'todo only filtered by subject, status, and tags'}, 400
+        return jsonify({'error': 'todo only filtered by subject, status, and tags'}), 400
     
-    if not query: return {'error': 'q query parameter is required'}, 400
+    if not query: return jsonify({'error': 'q query parameter is required'}), 400
 
     if type == 'tags':
         res = [item for item in todos if query in item.get('tags')]
-        return res if res else res, 404
+        if res:
+            return jsonify(res), 200
+        return jsonify(res), 404
     
     res = [item for item in todos if item.get(type) == query]
-    return res if res else res, 404
+    if res:
+        return jsonify(res), 200
+    return jsonify(res), 404
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
